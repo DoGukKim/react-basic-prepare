9f6fe7894be78ea95241bba7213d720aca6fdf48
DOGUK <dgkim3811@naver.com>
2020-08-20 13:30:54 +0900

    AddProp-TypesAndUseIt

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Screen from "./Screen";
+import PropsTypes from "prop-types";
+
 function App() {
   return (
     <>
-      <Screen name="React!">I like TS and React!</Screen>
+      <Screen name="React!" favNum={355435416351}>
+        I like TS and React!
+      </Screen>
     </>
   );
 }
 
+Screen.PropsTypes = {
+  name: PropsTypes.String,
+  favNum: PropsTypes.string.isRequired,
+};
 export default App;
--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,10 +1,11 @@
 import React, { children } from "react";
 
-const Screen = ({ name, children }) => {
+const Screen = ({ name, children, favNum }) => {
   return (
     <>
       <h1>Hellow {name}</h1>
       <p>{children}</p>
+      <p>{favNum}</p>
     </>
   );
 };
