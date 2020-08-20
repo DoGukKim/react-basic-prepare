8affa4e305785a08ae185a3d22a13abb0a5fc1a9
DOGUK <dgkim3811@naver.com>
2020-08-20 14:34:30 +0900

    UpdateUseingSetState

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import Screen from "./Screen";
-import PropsTypes from "prop-types";
 
 function App() {
   return (
     <>
-      <Screen name="React!" favNum={355435416351}>
-        I like TS and React!
-      </Screen>
+      <Screen />
     </>
   );
 }
 
-Screen.PropsTypes = {
-  name: PropsTypes.String,
-  favNum: PropsTypes.string.isRequired,
-};
 export default App;
--- src/Components/Screen.js
+++ src/Components/Screen.js
@@ -1,13 +1,45 @@
-import React, { children } from "react";
+import React, { Fragment } from "react";
+import PropsTypes from "prop-types";
 
-const Screen = ({ name, children, favNum }) => {
-  return (
-    <>
-      <h1>Hellow {name}</h1>
-      <p>{children}</p>
-      <p>{favNum}</p>
-    </>
-  );
+class Screen extends React.Component {
+  state = {
+    counter: 0,
+  };
+  add = () => {
+    this.setState(
+      {
+        counter: this.state.counter + 1,
+      },
+      // 추가적인 작업
+      () => console.log("StateAddIt!")
+    );
+  };
+  min = () => {
+    this.setState(prev => ({
+      counter: prev.counter - 1,
+    }));
+  };
+  render() {
+    const { counter } = this.state;
+    return (
+      <Fragment>
+        <h3>The Num is {counter}</h3>
+        <button onClick={this.add}>+1</button>
+        <button
+          onClick={() => {
+            this.setState(prev => ({
+              counter: prev.counter - 1,
+            }));
+          }}
+        >
+          -1
+        </button>
+      </Fragment>
+    );
+  }
+}
+Screen.PropsTypes = {
+  count: PropsTypes.number.isRequired,
 };
 
 export default Screen;
